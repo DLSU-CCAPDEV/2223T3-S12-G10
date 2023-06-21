2b1e52e9027250e6047abf3f57880a931a5ac645
lloyddominic26 <ademic26@yahoo.com>
2023-06-21 14:05:37 +0800

    Added Ask Question functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,176 @@
+function handleVoteButtons(e) {
+    console.log(e.currentTarget);
+    $(e.currentTarget).toggleClass('active');
+    let attr = $(e.currentTarget).parent().data('vote-status');
+    if (typeof attr !== 'undefined' || attr !== false) {
+        $(e.currentTarget).parent().data('vote-status', '0');
+    }
+    /*let vote_count = $(e.currentTarget).siblings('.post-vote-count');
+    let vote_count_attr = vote_count.attr('data-vote-count');
+    if (typeof vote_count_attr === 'undefined' || vote_count_attr !== false) {
+        vote_count.attr('data-vote-count', vote_count.text());
+    }*/
+}
+
+function handleVoteButtonUp(e) {
+    let parent = $(e.currentTarget).parent();
+    let vote_count = $(e.currentTarget).siblings('.post-vote-count');
+    let data_vote_count = vote_count.attr('data-vote-count');
+    if (parent.attr('data-vote-status') != 1) {
+        var diff = 1;
+        if (parent.attr('data-vote-status') == -1) diff = 2;
+        vote_count.text(nFormatter(parseInt(data_vote_count) + diff, 1));
+        vote_count.attr('data-vote-count', parseInt(data_vote_count) + diff);
+        parent.attr('data-vote-status', 1);
+        $(e.currentTarget).siblings('.post-vote-down').removeClass('active');
+    } else {
+        parent.attr('data-vote-status', 0);
+        vote_count.text(nFormatter(parseInt(data_vote_count) - 1, 1));
+        vote_count.attr('data-vote-count', parseInt(data_vote_count) - 1);
+    }
+}
+
+function handleVoteButtonsDown(e) {
+    let parent = $(e.currentTarget).parent();
+    let vote_count = $(e.currentTarget).siblings('.post-vote-count');
+    let data_vote_count = vote_count.attr('data-vote-count');
+    if (parent.attr('data-vote-status') != -1) {
+        var diff = 1;
+        if (parent.attr('data-vote-status') == 1) diff = 2;
+        vote_count.text(nFormatter(parseInt(data_vote_count) - diff, 1));
+        vote_count.attr('data-vote-count', parseInt(data_vote_count) - diff);
+        parent.attr('data-vote-status', -1);
+        $(e.currentTarget).siblings('.post-vote-up').removeClass('active');
+    } else {
+        parent.attr('data-vote-status', 0);
+        vote_count.text(nFormatter(parseInt(data_vote_count) + 1, 1));
+        vote_count.attr('data-vote-count', parseInt(data_vote_count) + 1);
+    }
+}
+
+function validateForm(...forms) {
+    let valid = true;
+    forms.forEach(form => {
+        if (!form.value.trim().length)
+            valid = false;
+            return valid;
+    });
+    return valid;
+}
+
 $(document).ready(function() {
-    $('.post-vote-up, .post-vote-down').click(function() {
-        $(this).toggleClass('active');
-        let attr = $(this).parent().attr('data-vote-status');
-        if (typeof attr !== 'undefined' || attr !== false) {
-            $(this).parent().data('vote-status', '0');
-        }
+    $('.post-vote-up, .post-vote-down').click(handleVoteButtons);
+    $('.post-vote-up').click(handleVoteButtonUp);
+    $('.post-vote-down').click(handleVoteButtonsDown);
+
+    $('#btn-ask').click(function() {
+        $('#post-title-form').val('');
+        $('#post-content-form').val('');
+        $('#post-tags-form').val('');
     });
-    $('.post-vote-up').click(function() {
-        let parent = $(this).parent();
-        let vote_count = $(this).siblings('.post-vote-count');
-        let data_vote_count = vote_count.attr('data-vote-count');
-        if (parent.attr('data-vote-status') != 1) {
-            var diff = 1;
-            if (parent.attr('data-vote-status') == -1) diff = 2;
-            vote_count.text(nFormatter(parseInt(data_vote_count) + diff, 1));
-            vote_count.attr('data-vote-count', parseInt(data_vote_count) + diff);
-            parent.attr('data-vote-status', 1);
-            $(this).siblings('.post-vote-down').removeClass('active');
-        } else {
-            parent.attr('data-vote-status', 0);
-            vote_count.text(nFormatter(parseInt(data_vote_count) - 1, 1));
-            vote_count.attr('data-vote-count', parseInt(data_vote_count) - 1);
-        }
-    });
-    $('.post-vote-down').click(function() {
-        let parent = $(this).parent();
-        let vote_count = $(this).siblings('.post-vote-count');
-        let data_vote_count = vote_count.attr('data-vote-count');
-        if (parent.attr('data-vote-status') != -1) {
-            var diff = 1;
-            if (parent.attr('data-vote-status') == 1) diff = 2;
-            vote_count.text(nFormatter(parseInt(data_vote_count) - diff, 1));
-            vote_count.attr('data-vote-count', parseInt(data_vote_count) - diff);
-            parent.attr('data-vote-status', -1);
-            $(this).siblings('.post-vote-up').removeClass('active');
-        } else {
-            parent.attr('data-vote-status', 0);
-            vote_count.text(nFormatter(parseInt(data_vote_count) + 1, 1));
-            vote_count.attr('data-vote-count', parseInt(data_vote_count) + 1);
-        }
+
+    $('#btn-post-form').click(function() {
+        const post_title_form = document.getElementById('post-title-form');
+        const post_content_form = document.getElementById('post-content-form');
+        const post_tags_form = document.getElementById('post-tags-form');
+
+        if (!validateForm(post_title_form, post_content_form, post_tags_form)) return;
+        $('#modal-question').modal('hide');
+
+        let post_title = post_title_form.value;
+        let post_content = post_content_form.value;
+        let post_tags = post_tags_form.value.split(',');
+
+        let post_wrapper = document.createElement("div");
+        post_wrapper.classList.add("post-wrapper");
+
+        let post_vote_container = document.createElement("div");
+        post_vote_container.classList.add("post-vote-container");
+
+        let post_vote_up = document.createElement("div");
+        post_vote_up.classList.add("post-vote-up");
+
+        let icon_vote_up = document.createElement("i");
+        icon_vote_up.classList.add("fa", "fa-caret-up");
+
+        post_vote_up.appendChild(icon_vote_up);
+        post_vote_up.addEventListener("click", handleVoteButtonUp);
+        post_vote_up.addEventListener("click", handleVoteButtons);
+
+        let post_vote_count = document.createElement("div");
+        post_vote_count.classList.add("post-vote-count");
+        post_vote_count.innerText = "0";
+        post_vote_count.setAttribute("data-vote-count", "0");
+
+        let post_vote_down = document.createElement("div");
+        post_vote_down.classList.add("post-vote-down");
+        post_vote_down.addEventListener("click", handleVoteButtonsDown);
+        post_vote_down.addEventListener("click", handleVoteButtons);
+
+        let icon_vote_down = document.createElement("i");
+        icon_vote_down.classList.add("fa", "fa-caret-down");
+
+        post_vote_down.appendChild(icon_vote_down);
+
+        post_vote_container.append(post_vote_up, post_vote_count, post_vote_down);
+
+        let post_container = document.createElement("div");
+        post_container.classList.add("post-container");
+
+        let post_title_el = document.createElement("h2");
+        post_title_el.classList.add("post-title");
+        post_title_el.innerText = post_title;
+
+        let post_author_container = document.createElement("div");
+        post_author_container.classList.add("post-author-container");
+
+        let post_author_img = document.createElement("img");
+        post_author_img.classList.add("post-author-img");
+        post_author_img.src = "https://api.dicebear.com/6.x/avataaars/svg?seed=Aaron+Hall";
+        post_author_img.alt = "@aaronhall";
+
+        let post_author = document.createElement("div");
+        post_author.classList.add("post-author");
+        post_author.innerText = "@aaronhall";
+
+        post_author_container.append(post_author_img, post_author);
+
+        let post_content_el = document.createElement("p");
+        post_content_el.innerHTML = post_content;
+
+        let footer_container = document.createElement("div");
+        footer_container.classList.add("footer-container");
+
+        let tags_container = document.createElement("div");
+        tags_container.classList.add("tags-container");
+
+        post_tags.forEach(function(tag) {
+            let post_tags_el = document.createElement("div");
+            post_tags_el.classList.add("tags");
+            post_tags_el.innerText = tag.trim();
+
+            tags_container.append(post_tags_el);
+        });
+
+        let post_answers_count_container = document.createElement("div");
+        post_answers_count_container.classList.add("post-answers-count-container");
+
+        let icon_comment = document.createElement("i");
+        icon_comment.classList.add("fa", "fa-comment-dots");
+
+        let post_answers_count = document.createElement("div");
+        post_answers_count.classList.add("post-answers-count");
+        post_answers_count.innerText = "0 answers";
+
+        post_answers_count_container.append(icon_comment, post_answers_count);
+
+        footer_container.append(tags_container, post_answers_count_container);
+
+        post_container.append(post_title_el, post_author_container, post_content_el, footer_container);
+
+        post_wrapper.append(post_vote_container, post_container);
+
+        let posts_container = $('#posts-container');
+        posts_container.prepend(post_wrapper);
     });
 });
