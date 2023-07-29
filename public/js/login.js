8effcedc5c3cc29f26f43ce4144ef09f8420f975
Lloyd26 <ademic26@gmail.com>
2023-07-30 02:14:56 +0800

    (For #13) Fixed layout of sign-up page, Added specific error messages for signing up

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,20 @@
-function validateLogin(username, password) {
+/*function validateLogin(username, password) {
     let valid = false;
     if (username == "admin@gmail.com" && password == "admin12345") {
         valid = true;
     }
 
     return valid;
+}*/
+
+function showError(error_text) {
+    let error_container = $('#error-container');
+    error_container.attr('data-error-status', 'error');
+    error_container.css('animation', 'shake ease-in-out 0.375s');
+    setTimeout(function() {
+        $('#error-container').css('animation', '');
+    }, 500);
+    error_container.text(error_text);
 }
 
 $(document).ready(function() {
@@ -12,28 +22,48 @@ $(document).ready(function() {
         let username = $('#username').val();
         let password = $('#password').val();
 
-        let validLogin = validateLogin(username, password);
+        //let validLogin = validateLogin(username, password);
         let validForm = validateForm(username, password);
 
-        if (!validLogin || !validForm) {
-            $('#error-container').attr('data-error-status', 'error');
-            $('#error-container').css('animation', 'shake ease-in-out 0.375s');
+        if (!validForm) {
             e.preventDefault();
-            setTimeout(function() {
-                $('#error-container').css('animation', '');
-            }, 500);
 
-            if (!validLogin) {
+            /*if (!validLogin) {
                 $('#error-container').text('Incorrect username or password!');
-            }
+            }*/
 
             if (!validForm) {
-                $('#error-container').text('Please enter a username and password!');
+                showError('Please enter a username and password!');
             }
             return;
         }
 
-        let formAction = $('#form-login').attr('action');
-        window.location.href = formAction;
+        /*let formAction = $('#form-login').attr('action');
+        window.location.href = formAction;*/
+    });
+
+    $('#form-register').on("submit", function(e) {
+        let username = $('#username').val();
+        let password = $('#password').val();
+        let confirm_password = $('#confirm_password').val();
+
+        if (!validateForm(username, password, confirm_password)) {
+            e.preventDefault();
+            showError('Please enter a username and password!');
+        } else if (password != confirm_password) {
+            e.preventDefault();
+            showError('Passwords do not match!');
+        } else if (password.length < 8) {
+            e.preventDefault();
+            showError('Password should contain at least 8 characters!');
+        }
+    });
+
+    $('#confirm_password').on("keyup", function() {
+        if ($(this).val() != $('#password').val()) {
+            $(this).addClass('form-input-error');
+        } else {
+            $(this).removeClass('form-input-error');
+        }
     });
 });
