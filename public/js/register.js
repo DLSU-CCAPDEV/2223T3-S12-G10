4b82d521373b4abc2aaf4553de7e6bfeba8a0f1d
Lloyd26 <ademic26@yahoo.com>
2023-08-03 17:10:32 +0800

    Added front-end string validation

--- public/js/register.js
+++ public/js/register.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
         if (!validateForm(username, password, confirm_password)) {
             e.preventDefault();
-            showError('Please enter a username and password!1');
+            showError('Please enter a username and password!');
         } else if (password != confirm_password) {
             e.preventDefault();
             showError('Passwords do not match!');
@@ -26,10 +26,31 @@ $(document).ready(function() {
             e.preventDefault();
             showError('Password should contain at least 8 characters!');
         }
+
+        let pattern = "^(?=.{3,16}$)[a-zA-Z0-9_]+$";
+        if (!username.match(pattern)) {
+            e.preventDefault();
+
+            if (username.length < 3) showError('Username is too short!');
+            else if (username.length > 16) showError('Username is too long!');
+            else if (username.indexOf(' ') >= 0) showError('Username cannot contain spaces!');
+            else showError('Username can only contain\nalphanumeric characters and underscores!');
+        }
     });
 
     $('#confirm_password').on("keyup", function() {
         if ($(this).val() != $('#password').val()) {
+            $(this).addClass('form-input-error');
+        } else {
+            $(this).removeClass('form-input-error');
+        }
+    });
+
+    $('#form-register #username').on("keyup", function() {
+        let pattern = "^(?=.{3,16}$)[a-zA-Z0-9_]+$";
+        let username = $(this).val();
+
+        if (!username.match(pattern)) {
             $(this).addClass('form-input-error');
         } else {
             $(this).removeClass('form-input-error');
